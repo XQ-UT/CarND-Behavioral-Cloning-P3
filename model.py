c04bb18d0e658cc1c2452b76a155f35c01027a4e
Xianlei Qiu <qiuxianlei@gmail.com>
2017-12-03 23:33:24 -0800

    add generator, test loss, use RGB images

--- model.py
+++ model.py
@@ -1,36 +1,74 @@
 import csv
 import cv2
+import math
 import numpy as np
 
-lines = []
-with open('./data/track1/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
 
-images = []
-measurements = []
-for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = './data/track1/IMG/' + filename
+def read_samples_from_csv(csv_file, samples):
+    with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile)
+        for sample in reader:
+            samples.append(sample)
 
-    image = cv2.imread(current_path)
-    measurement = float(line[3])
-    images.append(image)
-    measurements.append(measurement)
+from sklearn.utils import shuffle
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    # flip img
-    image_flipped = np.fliplr(image)
-    measurement_flipped = -measurement
-    images.append(image_flipped)
-    measurements.append(measurement_flipped)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i in range(0, 3):
+                    steering_bias = 0.0
+                    if i == 1:
+                        steering_bias = 0.2
+                    elif i == 2:
+                        steering_bias = -0.2
 
-X_train = np.array(images)
-y_train = np.array(measurements)
-print('X_train shape: {}'.format(X_train.shape))
-print('y_train shape: {}'.format(y_train.shape))
+                    image_path = batch_sample[i]
+                    image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+                    angle = steering_bias + float(batch_sample[3])
+                    images.append(image)
+                    angles.append(angle)
 
+                    # Flip Image
+                    image_flipped = np.fliplr(image)
+                    angle_flipped = - angle
+                    images.append(image_flipped)
+                    angles.append(angle_flipped)
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
+
+#---------------------------------------
+#  Generate Training, Validation and 
+#  Testing data.
+#--------------------------------------
+track1_csv_file = './data/track1/driving_log.csv'
+reversed_track1_csv_file = './data/track1_reversed/driving_log.csv'
+samples = []
+
+read_samples_from_csv(track1_csv_file, samples)
+read_samples_from_csv(reversed_track1_csv_file, samples)
+
+from sklearn.model_selection import train_test_split
+train_samples, test_samples = train_test_split(samples, test_size=0.1)
+train_samples, validation_samples = train_test_split(train_samples, test_size=0.2)
+print('Training sample size: 6 x {}'.format(len(train_samples)))
+print('Validation sample size: 6 x {}'.format(len(validation_samples)))
+print('Testing sample size: 6 x {}'.format(len(test_samples)))
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+test_generator = generator(test_samples, batch_size=32)
+
+#-----------------------------------------
+#  Model Definition
+#-----------------------------------------
 import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Conv2D, Lambda, Dropout, Activation, BatchNormalization, Cropping2D
@@ -39,20 +77,21 @@ from keras.optimizers import Adam
 print('Keras Version: {}'.format(keras.__version__))
 
 model = Sequential()
+# Crop image
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
 model.add(Lambda(lambda x: x/255.0 - 0.5))
 
-model.add(Conv2D(filters=10, kernel_size=[5,5], strides=[2,2], padding='valid'))
+model.add(Conv2D(filters=24, kernel_size=[5,5], strides=[2,2], padding='valid'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(Dropout(rate=0.2))
 
-model.add(Conv2D(filters=15, kernel_size=[5,5], strides=[2,2], padding='valid'))
+model.add(Conv2D(filters=36, kernel_size=[5,5], strides=[2,2], padding='valid'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(Dropout(rate=0.2))
 
-model.add(Conv2D(filters=20, kernel_size=[3,3], strides=[2,2], padding='valid'))
+model.add(Conv2D(filters=48, kernel_size=[3,3], strides=[2,2], padding='valid'))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(Dropout(rate=0.2))
@@ -63,10 +102,32 @@ model.add(Dense(256))
 model.add(Dense(64))
 model.add(Dense(1))
 
-adam = Adam(lr=0.001, decay=0.01)
-model.compile(loss='mean_squared_error', optimizer=adam)
-model.fit(X_train, y_train, batch_size = 128, epochs = 10, 
-    validation_split = 0.2, shuffle=True)
 
+#------------------------------------
+#  Train model
+#------------------------------------
+adam = Adam(lr=0.001)
+model.compile(loss='mean_squared_error', optimizer=adam)
+hist  = model.fit_generator(
+    generator = train_generator,
+    steps_per_epoch = math.ceil(len(train_samples) / 32.0),
+    validation_data = validation_generator,
+    validation_steps = math.ceil(len(validation_samples) / 32.0),
+    epochs = 20, 
+)
 model.save('model.h5')
+print('Training History: ')
+print(hist.history)
+
+
+#---------------------------------------
+#   Predict on test data 
+#----------------------------------------
+hist = model.predict_generator(
+    generator = test_generator,    
+    steps = math.ceil(len(test_samples) / 32.0),
+)
+print('Test History: ')
+print(hist.history)
+
 
